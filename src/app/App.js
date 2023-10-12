3bd573daabdeb565adda6b1199e5fbe817f2f0ba
hDn248 <hodangnhan24@gmail.com>
2023-10-12 14:01:46 +0700

    Move app component into app folder

---
+++ src/app/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import React from "react";
+
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+import Section from './components/section/Section';
+
+const App = () => {
+  return (
+    <>
+      <Header></Header>
+      <Section></Section>
+      <Footer></Footer>
+    </>
+  )
+}
+
+export default App;
