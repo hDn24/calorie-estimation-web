a09efd6b222f93afbe0ac0798fcefc39ea6c08a0
hDn248 <hodangnhan24@gmail.com>
2023-10-17 21:13:33 +0700

    Resolve conflict

--- src/app/components/header/Header.js
+++ src/app/components/header/Header.js
@@ -1,31 +1,31 @@
 const Header = () => {
     return (
-        <div>
-            <a href="#scroll-top" className="scrolltop" id="scroll-top">
-                <i className="bx bx-chevron-up scrolltop__icon" alt="Scroll to top"></i>
+        <>
+            <a href="#" class="scrolltop" id="scroll-top">
+                <i class='bx bx-chevron-up scrolltop__icon'></i>
             </a>
-            <header className="header" id="header">
-                <nav className="nav bd-container">
-                    <a href="#" className="nav__logo">Calories estimation</a>
+            <header class="l-header" id="header">
+                <nav class="nav bd-container">
+                    <a href="#" class="nav__logo">Calories estimation</a>
 
-                    <div className="nav__menu" id="nav-menu">
-                        <ul className="nav__list">
-                            <li className="nav__item"><button href="#home" className="nav__link active-link">Home</button></li>
-                            <li className="nav__item"><button href="#about" className="nav__link">About</button></li>
-                            <li className="nav__item"><button href="#services" className="nav__link">Services</button></li>
-                            <li className="nav__item"><button href="#menu" className="nav__link">Menu</button></li>
-                            <li className="nav__item"><button href="#contact" className="nav__link">Contact us</button></li>
+                    <div class="nav__menu" id="nav-menu">
+                        <ul class="nav__list">
+                            <li class="nav__item"><a href="#home" class="nav__link active-link">Home</a></li>
+                            <li class="nav__item"><a href="#about" class="nav__link">About</a></li>
+                            <li class="nav__item"><a href="#services" class="nav__link">Services</a></li>
+                            <li class="nav__item"><a href="#menu" class="nav__link">Menu</a></li>
+                            <li class="nav__item"><a href="#contact" class="nav__link">Contact us</a></li>
 
-                            <li><i className="bx bx-moon change-theme" id="theme-button" alt="Change theme"></i></li>
+                            <li><i class='bx bx-moon change-theme' id="theme-button"></i></li>
                         </ul>
                     </div>
 
-                    <div className="nav__toggle" id="nav-toggle">
-                        <i className="bx bx-menu"></i>
+                    <div class="nav__toggle" id="nav-toggle">
+                        <i class='bx bx-menu'></i>
                     </div>
                 </nav>
             </header>
-        </div>
+        </>
     )
 }
 
