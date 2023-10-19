97da65fd15f8e22d84c9f2d0a7081b49808a3416
hDn248 <hodangnhan24@gmail.com>
2023-10-19 14:59:26 +0700

    Minor fix

--- src/app/components/footer/Footer.tsx
+++ src/app/components/footer/Footer.tsx
@@ -20,68 +20,70 @@ const Footer = () => {
               </a>
             </div>
           </div>
-        </div>
-        <div className="footer__content">
-          <h3 className="footer__title">Services</h3>
-          <ul>
-            <li>
-              <a href="#" className="footer__link">
-                Delivery
-              </a>
-            </li>
-            <li>
-              <a href="#" className="footer__link">
-                Pricing
-              </a>
-            </li>
-            <li>
-              <a href="#" className="footer__link">
-                Fast estimate
-              </a>
-            </li>
-            <li>
-              <a href="#" className="footer__link">
-                Reserve your spot
-              </a>
-            </li>
-          </ul>
+
+          <div className="footer__content">
+            <h3 className="footer__title">Services</h3>
+            <ul>
+              <li>
+                <a href="#" className="footer__link">
+                  Delivery
+                </a>
+              </li>
+              <li>
+                <a href="#" className="footer__link">
+                  Pricing
+                </a>
+              </li>
+              <li>
+                <a href="#" className="footer__link">
+                  Fast estimate
+                </a>
+              </li>
+              <li>
+                <a href="#" className="footer__link">
+                  Reserve your spot
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          <div className="footer__content">
+            <h3 className="footer__title">Information</h3>
+            <ul>
+              <li>
+                <a href="#" className="footer__link">
+                  Event
+                </a>
+              </li>
+              <li>
+                <a href="#" className="footer__link">
+                  Contact us
+                </a>
+              </li>
+              <li>
+                <a href="#" className="footer__link">
+                  Privacy policy
+                </a>
+              </li>
+              <li>
+                <a href="#" className="footer__link">
+                  Terms of services
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          <div className="footer__content">
+            <h3 className="footer__title">Adress</h3>
+            <ul>
+              <li>DN - Viet Nam</li>
+              <li>hDn24</li>
+              <li>034 - 567 - 890</li>
+              <li>hodangnhan24@gmail.com</li>
+            </ul>
+          </div>
         </div>
 
-        <div className="footer__content">
-          <h3 className="footer__title">Information</h3>
-          <ul>
-            <li>
-              <a href="#" className="footer__link">
-                Event
-              </a>
-            </li>
-            <li>
-              <a href="#" className="footer__link">
-                Contact us
-              </a>
-            </li>
-            <li>
-              <a href="#" className="footer__link">
-                Privacy policy
-              </a>
-            </li>
-            <li>
-              <a href="#" className="footer__link">
-                Terms of services
-              </a>
-            </li>
-          </ul>
-        </div>
-
-        <div className="footer__content">
-          <h3 className="footer__title">Adress</h3>
-          <ul>
-            <li>DN - Viet Nam</li>
-            <li>hDn24</li>
-            <li>034 - 567 - 890</li>
-            <li>hodangnhan24@gmail.com</li>
-          </ul>
-        </div>
         <p className="footer__copy">&#169; 2023 hDn24. All right reserved</p>
       </footer>
     </>
