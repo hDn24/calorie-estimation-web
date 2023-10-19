d3c3b859116c6dd4bed0b4ed08277bcb91a33da6
hDn248 <hodangnhan24@gmail.com>
2023-10-19 14:47:12 +0700

    Convert code to ts

--- src/app/components/section/About.js
+++ src/app/components/section/About.js
@@ -1,26 +1,55 @@
 import { useState } from "react";
 
 const About = () => {
-    const [isImageVisible, setImageVisible] = useState(true);
+  const [isImageVisible, setImageVisible] = useState(true);
 
-    const toogleImage = () => {
-        setImageVisible(false);
-    };
+  const toogleImage = () => {
+    setImageVisible(false);
+  };
 
-    return (
-        <section class="about section bd-container" id="about">
-            <div class="about__container  bd-grid">
-                <div class="about__data">
-                    <span class="section-subtitle about__initial">About our services</span>
-                    <h2 class="section-title about__initial"> We accurately estimate <br /> the calorie content <br /> in the main dish</h2>
-                    <p class="about__description">We estimate the most accurate calorie content on your plate, with quick and accurate responses when you need to estimate calorie content, try our service</p>
-                    <div style={{ display: "flex", flexDirection: 'row' }}>
-                        <a class="button" onClick={(event) => { toogleImage(event) }}>Try to estimate</a>
-                    </div>
-                </div>
-                {isImageVisible ? <img src={require("../../../assets/image/raw_data.jpg")} alt="" class="about__img" /> : <img src={require("../../../assets/image/data.png")} alt="" class="about__img" />}
-            </div>
-        </section>
-    );
+  return (
+    <section className="about section bd-container" id="about">
+      <div className="about__container  bd-grid">
+        <div className="about__data">
+          <span className="section-subtitle about__initial">
+            About our services
+          </span>
+          <h2 className="section-title about__initial">
+            {" "}
+            We accurately estimate <br /> the calorie content <br /> in the main
+            dish
+          </h2>
+          <p className="about__description">
+            We estimate the most accurate calorie content on your plate, with
+            quick and accurate responses when you need to estimate calorie
+            content, try our service
+          </p>
+          <div style={{ display: "flex", flexDirection: "row" }}>
+            <a
+              className="button"
+              onClick={(event) => {
+                toogleImage(event);
+              }}
+            >
+              Try to estimate
+            </a>
+          </div>
+        </div>
+        {isImageVisible ? (
+          <img
+            src={require("../../../assets/image/raw_data.jpg")}
+            alt=""
+            className="about__img"
+          />
+        ) : (
+          <img
+            src={require("../../../assets/image/data.png")}
+            alt=""
+            className="about__img"
+          />
+        )}
+      </div>
+    </section>
+  );
 };
 export default About;
---
+++ src/app/components/section/Contact.tsx
@@ -0,0 +1,23 @@
+const Contact = () => {
+  return (
+    <section className="contact section bd-container" id="contact">
+      <div className="contact__container bd-grid">
+        <div className="contact__data">
+          <span className="section-subtitle contact__initial">Let's talk</span>
+          <h2 className="section-title contact__initial">Contact us</h2>
+          <p className="contact__description">
+            If you want to use our services, please contact us, and we will
+            respond promptly with our 24/7 live chat support
+          </p>
+        </div>
+
+        <div className="contact__button">
+          <a href="#" className="button">
+            Contact us now
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default Contact;
